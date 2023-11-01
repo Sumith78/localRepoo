c8f276983f008cd58f9b49c8766c91f0863ce2eb
Sumith <sumithrajput53361@gmail.com>
2023-11-01 18:07:37 +0530

    Added some files

---
+++ PrintFriendsName.py
@@ -0,0 +1,4 @@
+def PrintFriendsName(friend1, friend2, friend3):
+    print(f"Hi {friend3}, {friend2} and {friend1}")
+    
+PrintFriendsName("Mahesh", "Suresh" ," Davesh")
---
+++ PrintFriendsNames.java
@@ -0,0 +1,15 @@
+public class PrintFriendsNames {
+    public static void printFriendsNames(String friend1, String friend2, String friend3) {
+        System.out.println("Hi " + friend3 + ", " + friend2 + " and " + friend1);
+    }
+
+    public static void main(String[] args) {
+        String friend1 = "Mahesh";
+        String friend2 = "Suresh";
+        String friend3 = "Devesh";
+        
+        printFriendsNames(friend1, friend2, friend3);
+    }
+}
+
+
